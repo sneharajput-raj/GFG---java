f068a2849068d5af50aca727ac0a25f6df4f52f9
Sneha-Rajput <sneharajput7389@gmail.com>
2026-07-20 22:28:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Constructor/constructor.java
@@ -0,0 +1,11 @@
+// design a class User
+// instance variable: name(String)
+// Constructor: no parameter, initialize name to "Default"
+
+// code here
+class User{
+    String name;
+    User(){
+        name = "Default";
+    }
+}
