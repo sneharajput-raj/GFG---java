92b07f27dfd316f4db8f57855ef7d44eff43f972
Sneha-Rajput <sneharajput7389@gmail.com>
2026-07-20 22:33:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Constructor Overloading/constructor-overloading.java
@@ -0,0 +1,13 @@
+// design a class with two constructor
+// one default, the other parameterized
+
+// code here
+class User{
+    String name;
+    User(){
+        name = "Default";
+    }
+    User(String name){
+        this.name = name;
+    }
+}
