f80762053f95a8a6d2c95e04e458c2c53b6cc492
Sneha-Rajput <sneharajput7389@gmail.com>
2026-09-10 22:57:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Array/reverse-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
